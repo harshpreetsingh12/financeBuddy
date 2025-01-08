8f5153cdec74f53ff5dce86553f84703b3ee1bcc
unknown <problemsolver131@gmail.com>
2025-01-08 22:20:41 +0530

    build warnign ignore

---
+++ .eslint.rc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  };
+  
