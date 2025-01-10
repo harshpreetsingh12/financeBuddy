17f736d43ef2116d6c3bfec43e2c538ae0e08f32
unknown <problemsolver131@gmail.com>
2025-01-10 23:07:58 +0530

    landing link changes

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -43,7 +43,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/h59sYl1FR1g">
+          <Link href="https://www.youtube.com/h59sYl1FR1g" target="__blank">
             <Button size="lg" className="px-8" variant="outline">
               Watch Demo
             </Button>
